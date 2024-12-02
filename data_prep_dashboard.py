4bfba3bd405eca967b5df75ec346e260c0efa85d
uvgusakk <ulianahusak@gmail.com>
2024-12-02 11:44:06 +0100

    preparation of file

---
+++ data_prep_dashboard.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from datetime import datetime
+cr = pd.read_excel("customer_rating.xlsx")
+
+
+cr["TIMESTAMP"] = pd.to_datetime(cr["TIMESTAMP"], unit="s")
+cr["ISSUE_RESOLVED_ENUM"] = cr["ISSUE_RESOLVED_ENUM"].fillna("Unknown")
+cr["REGION"] = cr["REGION"].str.title()
+
+product_columns = [col for col in cr.columns if col.endswith("YN")]
+
+cr[product_columns] = cr[product_columns].replace({"Yes": 1, "No": 0})
+
+
+cr["TOTAL_PRODUCTS"] = cr[product_columns].sum(axis=1)
+
+# Export the modified DataFrame to an Excel file
+output = "Customer_Data_with_Products.xlsx"
+cr.to_excel(output, index=False)
+
+print(f"Data exported to {output}")
+
+
+
